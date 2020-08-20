14ac8b1a283cae6cee4cbab328d842f4e8fee7f5
Eiriknil <eirik.nilsen@unit.no>
2020-08-20 13:16:30 +0200

    script to create customer institutions

--- import_customers.py
+++ import_customers.py
@@ -1,44 +1,57 @@
 import boto3
 import json
 import copy
+import uuid
 
 client = boto3.client('dynamodb')
+customer_template_file_name = './customers/institution.json'
+test_customers_file_name = './customers/test_institutions.json'
 
 
 def scan_customers():
     response = client.scan(TableName='nva_customers')
 
-    for item in response['Items']:
-        print(item['identifier']['S'])
     return response['Items']
 
 
-def delete_item(identifier):
-    response = client.delete_item(TableName='nva_customers',
-                                  Key={'identifier': {
-                                      'S': identifier
-                                  }})
-    print(response)
+def delete_customers():
+    customers = scan_customers()
+    for customer in customers:
+        print(customer['identifier']['S'])
+        identifier = customer['identifier']['S']
+        if identifier.startswith('test_'):
+            response = client.delete_item(
+                TableName='nva_customers',
+                Key={'identifier': {
+                    'S': identifier
+                }})
+            print(response)
+    return
+
+
+def put_item(new_customer):
+
+    response = client.put_item(TableName='nva_customers', Item=new_customer)
     return response
 
 
-def put_item(administration_id, archive_name, feide_orgnization_id,
-             identifier):
+delete_customers()
 
-    with open(customer_template_file) as customer:
-        customer_template = json.load(customer)
-        new_customer = copy.deepcopy(customer_template)
-        new_customer['administrationId']['S'] = administration_id
-        new_customer['archiveName']['S'] = archive_name
-        new_customer['feideOrgnizationId']['S'] = feide_orgnization_id
-        new_customer['identifier']['S'] = identifier
+with open(customer_template_file_name) as customer_template_file:
+    customer_template = json.load(customer_template_file)
 
-        response = client.put_item(TableName='nva_customers',
-                                   Item=new_customer)
-        return response
+    with open(test_customers_file_name) as test_customers_file:
 
+        test_customers = json.load(test_customers_file)
+        for test_customer in test_customers:
+            print(test_customer)
+            new_customer = copy.deepcopy(customer_template)
+            new_customer['administrationId']['S'] = test_customer[
+                'administration_id']
+            new_customer['archiveName']['S'] = test_customer['archive_name']
+            new_customer['feideOrganizationId']['S'] = test_customer[
+                'feide_orgnization_id']
+            new_customer['identifier']['S'] = 'test_{}'.format(
+                str(uuid.uuid4()))
 
-test_administation_id = 'test@test.no'
-test_archive_name = 'test archive'
-test_feide_org_id = '0987654321'
-test_identifier = ''
+            result = put_item(new_customer)
